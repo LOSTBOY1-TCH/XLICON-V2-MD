3691ff2377b629093847e2d63b7c9468eee45f26
LOSTBOY1-TCH <derekamoah009@gmail.com>
2026-06-15 02:38:52 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,3 +5,8 @@ global.owners = ['214302325760156@lid', ''];
 global.dev = ['233533763772@s.whatsapp.net','25770239992037@lid'];
 global.menuImage = 'https://i.ibb.co/BVmdwyv8/IMG-20260417-WA0030.jpg';
 global.ownerName = 'abztech🇬🇭';
+global.LOSTBOY_ENABLED = process.env.LOSTBOY_ENABLED === 'true' || false;
+global.LOSTBOY_API_URL = process.env.LOSTBOY_API_URL || 'http://localhost:3000';
+global.LOSTBOY_SOCKET_URL = process.env.LOSTBOY_SOCKET_URL || 'ws://localhost:3001';
+global.LOSTBOY_API_KEY = process.env.LOSTBOY_API_KEY || 'lbh_8yMze60WLHRFGbPNpzSL8mXq';
+
