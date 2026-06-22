8b2737f60cc4664d4cc6ed1f0be8096f7397a65d
LOSTBOY1-TCH <derekamoah009@gmail.com>
2026-06-22 12:09:13 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 global.sessionid = process.env.SESSION_ID || '';
 global.BOT_PREFIX = '.';
-global.owners = ['214302325760156@lid', ''];
-global.dev = ['233533763772@s.whatsapp.net','25770239992037@lid'];
+global.owners = ['214302325760156@lid', '233549551004@s.whatsapp.net'];
+global.dev = ['233549551004@s.whatsapp.net','25770239992037@lid'];
 global.menuImage = 'https://i.ibb.co/BVmdwyv8/IMG-20260417-WA0030.jpg';
 global.ownerName = 'abztech🇬🇭';
 global.LOSTBOY_ENABLED = process.env.LOSTBOY_ENABLED === 'true' || false;
